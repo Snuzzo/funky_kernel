99a71a105400a21984888472e3b5efd805ea1fb9
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2012-04-12 11:33:14 +0530

    msm_fb: Make buffers aligned to 4K page boundary
    This change makes the frame buffers aligned to the page
    boundary. Improves GPU performance since accessing the
    frame buffer from page boundary is faster.
    
    Change-Id: I17879437dd7cbb9a1758d82321c6e68dbd88b9e0
    CRs-Fixed: 325134
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1412,13 +1412,11 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 
 	/* Make sure all buffers can be addressed on a page boundary by an x
 	 * and y offset */
-
-	remainder = (fix->line_length * panel_info->yres) & (PAGE_SIZE - 1);
-					/* PAGE_SIZE is a power of 2 */
+	remainder = (fix->line_length * panel_info->yres) % PAGE_SIZE;
 	if (!remainder)
 		remainder = PAGE_SIZE;
 	remainder_mode2 = (fix->line_length *
-				panel_info->mode2_yres) & (PAGE_SIZE - 1);
+				panel_info->mode2_yres) % PAGE_SIZE;
 	if (!remainder_mode2)
 		remainder_mode2 = PAGE_SIZE;
 
