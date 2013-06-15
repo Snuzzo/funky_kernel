d9da2d1f15b4875fe31fa3ddf61d40d7b7c53bd4
David Hays <dhays90@gmail.com>
2013-06-15 11:20:44 -0500

    Increase kernel SMI memory size Kernel SMI memory is increased to 6MB in order to run multiple instances of video. User space SMI memory size will be automatically reduced by size equal to increase in kernel SMI memory size.
    Change-Id: I207159d9b026bbbaaa3c149e9f0cb40a0b7585b0

--- arch/arm/mach-msm/board-vigor.c
+++ arch/arm/mach-msm/board-vigor.c
@@ -3087,7 +3087,7 @@ static void __init msm8x60_init_dsps(void)
 /* SMI PMEM Region, as the video core will use offset address */
 /* from the Firmware base */
 #define KERNEL_SMI_BASE			 (MSM_SMI_BASE)
-#define KERNEL_SMI_SIZE			 0x400000
+#define KERNEL_SMI_SIZE			 0x600000
 
 /* User space SMI PMEM Region for video core*/
 /* used for encoder, decoder input & output buffers  */
