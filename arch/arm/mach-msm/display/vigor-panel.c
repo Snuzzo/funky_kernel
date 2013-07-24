1574278611322c7ce878a0f800286eb968fbc2e4
David Hays <dhays90@gmail.com>
2013-07-24 00:36:55 -0500

    vigor: increase mdp_core_clk_rate
    Change-Id: I07d240dde7b79c403b5cebe8822e93a8e49cdd7f

--- arch/arm/mach-msm/display/vigor-panel.c
+++ arch/arm/mach-msm/display/vigor-panel.c
@@ -523,9 +523,9 @@ static struct mipi_dsi_platform_data mipi_pdata = {
 };
 
 int mdp_core_clk_rate_table[] = {
+	59080000,
 	128000000,
-	128000000,
-	200000000,
+	160000000,
 	200000000,
 };
 
@@ -547,7 +547,7 @@ static struct gamma_curvy gamma_tbl = {
 
 static struct msm_panel_common_pdata mdp_pdata = {
 	.gpio = 28,
-	.mdp_core_clk_rate = 128000000,
+	.mdp_core_clk_rate = 200000000,
 	.mdp_core_clk_table = mdp_core_clk_rate_table,
 	.num_mdp_clk = ARRAY_SIZE(mdp_core_clk_rate_table),
 #ifdef CONFIG_MSM_BUS_SCALING
