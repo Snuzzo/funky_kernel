f47c7d9366c21f70d93ec66dcc5fcaa56c3cfccf
Snuzzo <jrracinfan@gmail.com>
2013-07-27 08:31:35 -0400

    devices-msm8x60; Updated GPU clock array

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -941,18 +941,19 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 0,
 		},
 		{
-			.gpu_freq = 200000000,
+			.gpu_freq = 228571000,
 			.bus_freq = 2,
-			.io_fraction = 100,
+			.io_fraction = 33,
 		},
 		{
-			.gpu_freq = 177778000,
+			.gpu_freq = 200000000,
 			.bus_freq = 1,
 			.io_fraction = 100,
 		},
 		{
-			.gpu_freq = 27000000,
+			.gpu_freq = 177778000,
 			.bus_freq = 0,
+			.io_fraction = 100,
 		},
 	},
 	.init_level = 0,
@@ -970,31 +971,28 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
 			.gpu_freq = 266667000,
-			.bus_freq = 4,
+			.bus_freq = 3,
 			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 228571000,
-			.bus_freq = 3,
+			.bus_freq = 2,
 			.io_fraction = 33,
 		},
 		{
 			.gpu_freq = 200000000,
-			.bus_freq = 2,
-			.io_fraction = 100,
-		},
-		{
-			.gpu_freq = 177778000,
 			.bus_freq = 1,
 			.io_fraction = 100,
 		},
 		{
-			.gpu_freq = 27000000,
+			.gpu_freq = 177778000,
 			.bus_freq = 0,
+			.io_fraction = 100,
 		},
+
 	},
 	.init_level = 0,
-	.num_levels = 5,
+	.num_levels = 4,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/5,
 	.nap_allowed = true,
